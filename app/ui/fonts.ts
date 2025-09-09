e13401825308695b25063d5783ee217c8982ca09
Andie Similon <andie.similon@ap.be>
2025-09-09 10:44:05 +0200

    fix fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Notable, Inter } from "next/font/google";
+import { Notable, Inter, Lusitana, Roboto } from "next/font/google";
 
-export const notable = Notable({weight: "400", subsets: ["latin"], preload: false});
+export const notable = Notable({subsets: ["latin"], weight: "400"});
 export const inter = Inter({subsets: ["latin"]});
+export const lusitana = Lusitana({weight: "700", subsets: ["latin"]});
