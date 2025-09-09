1914ed9407056b42de2f6f68c2c6eec91f9f8457
Andie Similon <andie.similon@ap.be>
2025-09-09 10:30:21 +0200

    latin

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Notable, Inter } from "next/font/google";
 
-export const notable = Notable({weight: "400"});
+export const notable = Notable({weight: "400", subsets: ["latin"]});
 export const inter = Inter({subsets: ["latin"]});
