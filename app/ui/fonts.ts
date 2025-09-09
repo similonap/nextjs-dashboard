2ec2a051cefec2dcc58ce1039eb76665a496b254
Andie Similon <andie.similon@ap.be>
2025-09-09 10:42:19 +0200

    preload flase

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Notable, Inter } from "next/font/google";
 
-export const notable = Notable({weight: "400", subsets: ["latin"]});
+export const notable = Notable({weight: "400", subsets: ["latin"], preload: false});
 export const inter = Inter({subsets: ["latin"]});
