db2d481afa47c9de3116e32850414673340630e0
Andie Similon <andie.similon@ap.be>
2025-09-09 10:21:26 +0200

    remove lusitanan

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,4 @@
-import { Notable, Inter, Lusitana, Roboto } from "next/font/google";
+import { Notable, Inter } from "next/font/google";
 
 export const notable = Notable({weight: "400"});
 export const inter = Inter({subsets: ["latin"]});
-export const lusitana = Lusitana({weight: "700"});
