10962c0c6444a681be4292e19e65e38cec771bcd
Andie Similon <andie.similon@ap.be>
2025-09-09 10:16:37 +0200

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Notable, Inter, Lusitana, Roboto } from "next/font/google";
+
+export const notable = Notable({weight: "400"});
+export const inter = Inter({subsets: ["latin"]});
+export const lusitana = Lusitana({weight: "700"});
